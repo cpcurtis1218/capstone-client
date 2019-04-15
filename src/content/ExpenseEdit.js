72053efe2a4229765c1743e01e475b5b0ca45e4f
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-04-15 17:05:59 -0400

    Add expense Edit component

---
+++ src/content/ExpenseEdit.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import apiUrl from '../apiConfig'
+// import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router'
+
+class ExpenseEdit extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      expense: {
+        amount: '',
+        category: '',
+        description: ''
+      },
+      updated: false,
+      message: null
+    }
+  }
+
+  componentDidMount () {
+    const id = this.props.match.params.id
+    axios.get(`${apiUrl}/expenses/${id}`)
+      .then(response => this.setState({
+        expense: response.data.expense
+      }))
+      .catch(console.log)
+  }
+
+  handleChange = (event) => {
+    this.setState({ expense: {
+      ...this.state.expense, [event.target.name]: event.target.value
+    } })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    // destructuring the expense object
+    const { expense } = this.state
+
+    axios({
+      url: `${apiUrl}/expenses/${expense.id}`,
+      method: 'patch',
+      data: { expense }
+    })
+      .then(() => this.setState({ updated: true }))
+      .catch(() => this.setState({
+        expense: { ...expense, title: '', director: '', year: '' },
+        message: 'Update failed, please fill out forms and try again.'
+      }))
+  }
+
+  render () {
+    const { expense, updated } = this.state
+    if (updated) {
+      return <Redirect to={{
+        pathname: `/expenses/${expense.id}`,
+        state: { message: 'Expense Updated.' }
+      }}/>
+    } else {
+      const { amount, category, description } = expense
+      return (
+        <div>
+          <h3>Create a new Expense!</h3>
+          <form className="p-2" onSubmit={this.handleSubmit}>
+            <label htmlFor="amount">Amount:</label>
+            <input required={true} value={amount} type="number" name="amount" className="m-1" onChange={this.handleChange} />
+            <label htmlFor="category">Category:</label>
+            <input required={true} value={category} name="category" className="m-1" onChange={this.handleChange} />
+            <label htmlFor="description">Description:</label>
+            <input value={description} name="description" className="m-1" onChange={this.handleChange} />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.props.location.state ? this.props.location.state.message : ''}</p>
+        </div>
+      )
+    }
+  }
+}
+
+export default ExpenseEdit
