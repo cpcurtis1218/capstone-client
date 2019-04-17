9763b0ae84d7afcb87ff1569d1dac4ff5e2ae249
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-04-17 15:37:12 -0400

    Add update to links
    - Change Link to NavLink
    - Add activeClass
    - Remove Button

--- src/content/Home.js
+++ src/content/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { ButtonGroup, Button } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import { ButtonGroup } from 'react-bootstrap'
 
 class Expenses extends Component {
   render () {
@@ -8,8 +8,8 @@ class Expenses extends Component {
       return (
         <div className="homenav">
           <ButtonGroup size='lg' className="m-2 justify-content-center">
-            <Link to='/expenses'><Button className="mx-4" variant="dark" size="lg">View Expenses</Button></Link>
-            <Link to='/expenses-create'><Button className="mx-4" variant="dark" size="lg">New Expense</Button></Link>
+            <NavLink to='/expenses' className="btn" activeClassName="selected">View Expenses</NavLink>
+            <NavLink to='/expenses-create' className="btn" activeClassName="selected">New Expense</NavLink>
           </ButtonGroup>
         </div>
       )
