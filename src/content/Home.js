203524ebbfffd1b3278c203ca266351607dd9893
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-04-17 11:53:31 -0400

    Add user object, conditional
    - If user exists, show buttons for view and create
    - If no user, show message to log in

--- src/content/Home.js
+++ src/content/Home.js
@@ -4,14 +4,22 @@ import { ButtonGroup, Button } from 'react-bootstrap'
 
 class Expenses extends Component {
   render () {
-    return (
-      <div className="homenav">
-        <ButtonGroup size='lg' className="m-2 justify-content-center">
-          <Link to='/expenses'><Button className="mx-4" variant="dark" size="lg">View Expenses</Button></Link>
-          <Link to='/expenses-create'><Button className="mx-4" variant="dark" size="lg">New Expense</Button></Link>
-        </ButtonGroup>
-      </div>
-    )
+    if (this.props.user) {
+      return (
+        <div className="homenav">
+          <ButtonGroup size='lg' className="m-2 justify-content-center">
+            <Link to='/expenses'><Button className="mx-4" variant="dark" size="lg">View Expenses</Button></Link>
+            <Link to='/expenses-create'><Button className="mx-4" variant="dark" size="lg">New Expense</Button></Link>
+          </ButtonGroup>
+        </div>
+      )
+    } else {
+      return (
+        <div className="homenav">
+          <h3>Please login to track your expenses!</h3>
+        </div>
+      )
+    }
   }
 }
 
