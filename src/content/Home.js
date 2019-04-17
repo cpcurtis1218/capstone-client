6ac658a9a238ea31b90b05ca3480f91a45f756a9
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-04-17 09:24:19 -0400

    Add Spinner to div to prevent weird ui interaction

--- src/content/Home.js
+++ src/content/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { ButtonGroup, Button } from 'react-bootstrap'
 
 class Expenses extends Component {
   render () {
     return (
-      <div>
-        <h3>Welcome Home</h3>
-        <p>To view your expenses, please click <Link to='/expenses'><button>here</button></Link></p>
-        <p>To add a new expense, please click <Link to='/expenses-create'><button>here</button></Link></p>
+      <div className="homenav">
+        <ButtonGroup size='lg' className="m-2 justify-content-center">
+          <Link to='/expenses'><Button className="mx-4" variant="dark" size="lg">View Expenses</Button></Link>
+          <Link to='/expenses-create'><Button className="mx-4" variant="dark" size="lg">New Expense</Button></Link>
+        </ButtonGroup>
       </div>
     )
   }
