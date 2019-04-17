78d0ed093a568f86e08c50c9d6cd47a55ad8d3fe
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-04-17 15:35:11 -0400

    Add update to make forms appear vertically
    - Add divs to forms

--- src/content/ExpenseCreate.js
+++ src/content/ExpenseCreate.js
@@ -63,17 +63,25 @@ class ExpenseCreate extends Component {
     } else {
       const { amount, category, description, chargeDate } = expense
       return (
-        <div>
+        <div className="expense-form">
           <h3>Create a new Expense!</h3>
           <form className="p-2" onSubmit={this.handleSubmit}>
-            <label htmlFor="date">Date:</label>
-            <input required={true} value={chargeDate} type="date" name="chargeDate" className="m-1" onChange={this.handleChange} />
-            <label htmlFor="amount">Amount: $</label>
-            <input required={true} value={amount} type="number" name="amount" className="m-1" onChange={this.handleChange} />
-            <label htmlFor="category">Category:</label>
-            <input required={true} value={category} name="category" className="m-1" onChange={this.handleChange} />
-            <label htmlFor="description">Description:</label>
-            <input value={description} name="description" className="m-1" onChange={this.handleChange} />
+            <div>
+              <label htmlFor="date">Date:</label>
+              <input required={true} value={chargeDate} type="date" name="chargeDate" className="m-1" onChange={this.handleChange} />
+            </div>
+            <div>
+              <label htmlFor="amount">Amount: $</label>
+              <input required={true} value={amount} type="number" name="amount" className="m-1" onChange={this.handleChange} />
+            </div>
+            <div>
+              <label htmlFor="category">Category:</label>
+              <input required={true} value={category} name="category" className="m-1" onChange={this.handleChange} />
+            </div>
+            <div>
+              <label htmlFor="description">Description:</label>
+              <input value={description} name="description" className="m-1" onChange={this.handleChange} />
+            </div>
             <button type="submit">Submit</button>
           </form>
         </div>
