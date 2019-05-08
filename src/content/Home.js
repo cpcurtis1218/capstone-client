f5fea2cec1061bb31a96b36e38100aef723b8dce
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-05-08 18:28:34 -0400

    Remove Buttongroup from sidebar

--- src/content/Home.js
+++ src/content/Home.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-import { ButtonGroup } from 'react-bootstrap'
 
 class Expenses extends Component {
   render () {
     if (this.props.user) {
       return (
         <div className="homenav">
-          <ButtonGroup size='lg' className="m-2 justify-content-center">
-            <NavLink exact to='/' className="btn homenav-btn mx-1" activeClassName="selected">Home</NavLink>
-            <NavLink exact to='/expenses' className="btn homenav-btn mx-1" activeClassName="selected">View Expenses</NavLink>
-            <NavLink exact to='/expenses-create' className="btn homenav-btn mx-1" activeClassName="selected">New Expense</NavLink>
-          </ButtonGroup>
+          <NavLink exact to='/' className="homenav-btn" activeClassName="selected">Home</NavLink>
+          <NavLink exact to='/expenses' className="homenav-btn" activeClassName="selected">View Expenses</NavLink>
+          <NavLink exact to='/expenses-create' className="homenav-btn" activeClassName="selected">New Expense</NavLink>
         </div>
       )
     } else {
